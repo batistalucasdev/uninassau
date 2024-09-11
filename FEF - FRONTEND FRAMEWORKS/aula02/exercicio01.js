d335c4901acafea169628dd89d3b5ecd64478629
Lucas Batista <batistalucasdev@gmail.com>
2024-09-10 22:46:47 -0300

    2024.09.10

--- FEF - FRONTEND FRAMEWORKS/aula02/exercicio01.js
+++ FEF - FRONTEND FRAMEWORKS/aula02/exercicio01.js
@@ -1,16 +1,10 @@
-var nota1 = prompt("Digite a nota 1: ");
-
-document.write("\nNota 1: " + nota1 + " + " + "<br>");
-var nota2 = prompt("Digite a nota 2: ");
-document.write("\nNota 1: " + nota2 + " + " + "<br>");
-var nota3 = prompt("Digite a nota 3: ");
-document.write("\nNota 1: " + nota3 + " + " + "<br>");
+var nota1 = parseInt(prompt("Digite a nota 1: "));
+document.write("\nNota 1: " + nota1 + "<br>");
+var nota2 = parseInt(prompt("Digite a nota 2: "));
+document.write("\nNota 1: " + nota2 + "<br>");
+var nota3 = parseInt(prompt("Digite a nota 3: "));
+document.write("\nNota 1: " + nota3 + "<br>");
 
 let media = (nota1 + nota2 + nota3)/3;
 
-if (media > 6) {
-    document.write("O aluno está aprovado! Média = " + media);
-}else {
-    if (media <= 6)
-
-}
+document.write("\tMédia = " + media);
