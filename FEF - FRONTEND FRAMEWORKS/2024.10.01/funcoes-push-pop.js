20be3be1e35a2e33df72b25058ba7da9159416cb
Lucas Batista <batistalucasdev@gmail.com>
2024-10-01 09:55:14 -0300

    2024.10.01

---
+++ FEF - FRONTEND FRAMEWORKS/2024.10.01/funcoes-push-pop.js
@@ -0,0 +1,13 @@
+let v = [10, 20, 30];
+
+document.writeln(v);
+document.write("<br>");
+v.push(40);
+document.writeln(v);
+document.write("<br>");
+v.pop();
+document.writeln(v);
+document.write("<br>");
+
+let array = [a, b, c];
+document.writeln(array);
---
+++ FEF - FRONTEND FRAMEWORKS/2024.10.01/funcoes-shift.js
@@ -0,0 +1,23 @@
+let v = [10, 20, 30];
+
+document.writeln(v);
+document.write("<br>");
+v.shift();
+document.writeln(v);
+document.write("<br>");
+v.unshift(10);
+document.writeln(v);
+document.write("<br>");
+
+document.write("<br>");
+
+let array = ["a", "b", "c"];
+
+document.writeln(array);
+document.write("<br>");
+array.shift();
+document.writeln(array);
+document.write("<br>");
+array.unshift("a");
+document.writeln(array);
+document.write("<br>");
