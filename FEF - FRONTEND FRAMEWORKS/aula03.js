e011673650a91abadd974e8b910489d6fe1ac909
Lucas Batista <batistalucasdev@gmail.com>
2024-10-05 09:53:51 -0300

    2024.10.05

--- FEF - FRONTEND FRAMEWORKS/aula03.js
+++ FEF - FRONTEND FRAMEWORKS/aula03.js
@@ -53,20 +53,61 @@ function calcular_media() {
 
     let resultado05 = document.getElementById("resultado05");
     resultado05.innerHTML = `A média desses números é ${media.toFixed(2)}`;
-    
+
     if (media > 6) {
-        
+
         imprimir("resultado05", `O aluno está aprovado.`);
     }
 
-    
+
 
 }
 
 //Slide 13 - Exercício 02
+function eh_par() {
+    let numero = parseInt(document.getElementById("input-numero").value);
+    
+    let resultado06 = document.getElementById("resultado06");
+    resultado06.innerHTML = ` ${numero}`;
+
+    if (numero % 2 == 0) {
+        imprimir("resultado06", `O número é par.`);
+    }
+    else {
+        imprimir("resultado06", `O número é ímpar.`);
+    }
+}
 
 //Slide 14 - Exercício 01
+function custo_carro(){
+    let percentualDistribuidor = 0.28;
+    let percentualImpostos = 0.45;
+    let custoFabrica = parseFloat(document.getElementById("input-custo-carro").value);
+
+    let valorDistribuidor = custoFabrica * percentualDistribuidor;
+    let valorImpostos = custoFabrica * percentualImpostos;
+    
+    let custoFinal = custoFabrica + valorDistribuidor + valorImpostos;
+
+    let resultado07 = document.getElementById("resultado07");        resultado07.innerHTML = `O custo final ao consumidor é: R$ ${custoFinal.toFixed(2)}`;
+}
+
 //Slide 14 - Exercício 02
+function eh_positivo(){
+    let numero = parseInt(document.getElementById("input-numero").value);
+    
+    let resultado06 = document.getElementById("resultado06");
+    resultado06.innerHTML = ` ${numero}`;
+
+    if (numero % 2 == 0) {
+        imprimir("resultado06", `O número é par.`);
+    }
+    else {
+        imprimir("resultado06", `O número é ímpar.`);
+    }
+
+}
+
 //Slide 14 - Exercício 03
 
 //Slide 17 - Exercício 01
