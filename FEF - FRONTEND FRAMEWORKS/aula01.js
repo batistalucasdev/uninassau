b8829bb61b3e70c2d4371a50c4dfb6c19de5752c
Lucas Batista <batistalucasdev@gmail.com>
2024-10-05 14:54:15 -0300

    2024.10.05

--- FEF - FRONTEND FRAMEWORKS/aula01.js
+++ FEF - FRONTEND FRAMEWORKS/aula01.js
@@ -1,15 +1,21 @@
 // Lucas Soares Batista - 01678004
 // Aula 01
 
-//Slide 22 - Exercício 01
+//Slide 35 - Exercício 01
 
-function media() {
-    let nota01 = parseFloat(document.getElementById("input-nota01").value);
-    let nota02 = parseFloat(document.getElementById("input-nota02").value);
-    let nota03 = parseFloat(document.getElementById("input-nota03").value);
+function criarAlert() {
+    alert("Olá, bem vindo");
+}
 
-    let media = (nota01 + nota02 + nota03)/3;
+//Slide 35 - Exercício 02
+function permanecerPagina(){
+    confirm("Deseja permanecer na página?");
+}
 
-    let resultado01 = document.getElementById("resultado01");
-    resultado01.innerHTML = `A média desses elementos é: ${media.toFixed(1)}`;
+//Slide 35 - Exercício 03
+function inserirNome(){
+    let nome = prompt("Digite seu nome: ");
+
+    let resultado03 = document.getElementById("resultado03");
+    resultado03.innerHTML = `Seu nome é ${nome}`;
 }
