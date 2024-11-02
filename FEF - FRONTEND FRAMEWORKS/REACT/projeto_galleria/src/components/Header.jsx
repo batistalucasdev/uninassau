1174c55a9cb908bd07617cf4460dc7a294b202f9
Lucas Batista <batistalucasdev@gmail.com>
2024-11-02 15:33:03 -0300

    2024.11.02

---
+++ FEF - FRONTEND FRAMEWORKS/REACT/projeto_galleria/src/components/Header.jsx
@@ -0,0 +1,52 @@
+export default function Header() {
+   
+    return (
+        <header class="header">
+            <a href="" class="header-brand">Galleria</a>
+            <nav class="header-navbar">
+                <ul>
+                    <li>
+                        <a href="#" class="active">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Fotos</a>
+                    </li>
+                    <li>
+                        <a href="#">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="header-social-media">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+
+    )
+}
